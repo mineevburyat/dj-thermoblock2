e1f64576cc3e2b829d4fec351ec9f266101ddbd9
Mineev Buryat <mineev.buryat@gmail.com>
2026-01-07 18:11:54 +0800

    фигней занимался

--- apps/home4/urls.py
+++ apps/home4/urls.py
@@ -4,5 +4,5 @@ from django.views.generic.base import TemplateView
 app_name = 'home2'
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home/index.html'), name='index'),
+    path('', TemplateView.as_view(template_name='start.html'), name='index'),
 ]
