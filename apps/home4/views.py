e653672f41b23b3c1a5816291e4543fd888ca6b6
Mineev Buryat <mineev.buryat@gmail.com>
2026-01-08 20:30:52 +0800

    demo deploy

--- apps/home4/views.py
+++ apps/home4/views.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     """Главная страница"""
-    template_name = 'home/index1.html'
+    template_name = 'home/index.html'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
